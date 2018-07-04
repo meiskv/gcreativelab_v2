44d5e201af34783dc676f81f66e2f32978550037
Mikhail Villamor <miks.villamor@yahoo.com>
2018-07-05 00:42:26 +0400

    homepage

--- gcreativelab-home.php
+++ gcreativelab-home.php
@@ -41,20 +41,58 @@ get_header(); ?>
 										<div class="swiper-wrapper">
 											<div class="swiper-slide main-slide">
 												
+												<!--  -->
+									<div class="wrapper home">
+                                    <div class="left"> <!--  Left Starts -->
+                                        <div class="content__holder">
+                                            <div class="home__content__header">Strategy</div>
+                                            <div class="home__content__header">Design</div>
+											<div class="home__content__header">Production</div>
+                                            <div class="home__content__btn">
+                                                <button>VISIT OUR CASE STUDY</button>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="address">INFO@GRAYLING.COM</div>
+                                        <div class="content__hero">G</div>
+                                    </div> <!--  Left Ends -->
+                                </div>
+								<!--  -->
 												<!-- Main Slide Content -->
 												<div class="address">INFO@GRAYLING.COM</div>
 												<div class="explore">DISCOVER THE STUDIO
 													  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/explore_arrow.svg" width="10" height="9" alt="">
 												</div>
 											</div>
-											<div class="swiper-slide home-cs-slide">Slide 1</div>
-											<div class="swiper-slide home-cs-slide">Slide 2</div>
-											<div class="swiper-slide home-cs-slide">Slide 3</div>
-											<div class="swiper-slide home-cs-slide">Slide 4</div>
-											<div class="swiper-slide home-cs-slide">Slide 5</div>
+											<?php 
+												for ($x = 1; $x <=5; $x++) {
+												?>
+											<div class="swiper-slide">
+													<div class="case__home__holder">
+														<div class="case__home__img">
+															<img src="<?php echo get_template_directory_uri(); ?>/assets/images/case__hero.jpg" alt="">
+														</div>
+														<div class="case__home__category">
+															<h4>CASE STUDY</h4>
+														</div>
+														<div class="case__home__meta">
+																<div class="case__home__date">
+																	<h4>June 26 - 2018</h4>
+																</div>
+																<div class="case__home__title"><h1>MEYDAN EQUINE</h1></div>
+																<div class="case__home__btn">
+																	<button>READ MORE</button>
+																</div>
+														</div>
+													<div class="case__home__number">.0<?php echo $x; ?></div>
+													</div>
+												</div>
+											<?php
+											}
+											?>
 										</div>
 										<!-- Add Pagination -->
-										<div class="swiper-pagination"></div>
+										<!-- <div class="swiper-pagination"></div> -->
 									</div>
 								<!--  -->
 						</div>

817157254d2151683d98312a09958a102cd1f221
Mikail Villamor <miks.villamor@yahoo.com>
2018-07-16 18:22:56 +0400

    disable parallax

--- assets/scripts/js/moveText.js
+++ assets/scripts/js/moveText.js
@@ -10,37 +10,37 @@
         TweenLite.to(elem,0.5,{x: amountMovedX,y: amountMovedY})
     });
 
-    $(window).mousemove(function(e){
+    // $(window).mousemove(function(e){
 
-        var amountMovedX = (e.pageX * -1 / 15);
-        var amountMovedY = (e.pageY * -1 / 15);
-        var elem0 = document.querySelector('#P0');
-        var elem1 = document.querySelector('#P1');
-        var elem2 = document.querySelector('#P2');
-        var elem3 = document.querySelector('#P3');
-        var elem4 = document.querySelector('#P4');
-        var elem5 = document.querySelector('#P5');
-        var elem6 = document.querySelector('#P6');
-        var elem7 = document.querySelector('#P7');
+    //     var amountMovedX = (e.pageX * -1 / 15);
+    //     var amountMovedY = (e.pageY * -1 / 15);
+    //     var elem0 = document.querySelector('#P0');
+    //     var elem1 = document.querySelector('#P1');
+    //     var elem2 = document.querySelector('#P2');
+    //     var elem3 = document.querySelector('#P3');
+    //     var elem4 = document.querySelector('#P4');
+    //     var elem5 = document.querySelector('#P5');
+    //     var elem6 = document.querySelector('#P6');
+    //     var elem7 = document.querySelector('#P7');
 
-        var cloud01 = document.querySelector('#Cloud_01');
-        var cloud02 = document.querySelector('#Cloud_02');
-        var cloud03 = document.querySelector('#Cloud_03');
-        // $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
-        TweenLite.to(cloud01,1.5,{x: -amountMovedX})
-        TweenLite.to(cloud02,1.5,{x: amountMovedX})
-        TweenLite.to(cloud03,1.5,{x: -amountMovedX})
+    //     var cloud01 = document.querySelector('#Cloud_01');
+    //     var cloud02 = document.querySelector('#Cloud_02');
+    //     var cloud03 = document.querySelector('#Cloud_03');
+    //     // $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+    //     TweenLite.to(cloud01,1.5,{x: -amountMovedX})
+    //     TweenLite.to(cloud02,1.5,{x: amountMovedX})
+    //     TweenLite.to(cloud03,1.5,{x: -amountMovedX})
 
-        TweenLite.to(elem0,2.9,{x: -amountMovedX})
-        TweenLite.to(elem1,1.7,{x: amountMovedX})
-        TweenLite.to(elem2,1.5,{x: -amountMovedX})
-        TweenLite.to(elem3,1.2,{x: amountMovedX})
-        TweenLite.to(elem4,0.9,{x: -amountMovedX})
-        TweenLite.to(elem5,0.6,{x: amountMovedX})
-        TweenLite.to(elem6,0.3,{x: -amountMovedX})
-        TweenLite.to(elem7,0,{x: amountMovedX})
+    //     TweenLite.to(elem0,2.9,{x: -amountMovedX})
+    //     TweenLite.to(elem1,1.7,{x: amountMovedX})
+    //     TweenLite.to(elem2,1.5,{x: -amountMovedX})
+    //     TweenLite.to(elem3,1.2,{x: amountMovedX})
+    //     TweenLite.to(elem4,0.9,{x: -amountMovedX})
+    //     TweenLite.to(elem5,0.6,{x: amountMovedX})
+    //     TweenLite.to(elem6,0.3,{x: -amountMovedX})
+    //     TweenLite.to(elem7,0,{x: amountMovedX})
 
-    });
+    // });
 
     }
     

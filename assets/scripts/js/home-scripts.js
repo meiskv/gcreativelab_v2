63a1b5f96b0aa3cdf6c73433fb338d463739e51c
Mikail Villamor <miks.villamor@yahoo.com>
2018-07-18 15:43:14 +0400

    menu

--- assets/scripts/js/home-scripts.js
+++ assets/scripts/js/home-scripts.js
@@ -42,12 +42,18 @@ window.requestAnimationFrame(function() {
                   let src = 'http://www.gcreativelab.com/wp-content/themes/gcreativelab_v2/assets/images/gcreative_logo_white.svg';
                   $('.glogo').attr('src', src);
 
+                  let menuIcon = 'http://www.gcreativelab.com/wp-content/themes/gcreativelab_v2/assets/images/menu-close.svg';
+                  $('.glogo').attr('src', menuIcon);
+
                   TweenMax.staggerFromTo('#menu li a', 0.1,{x: '-140',autoAlpha:0},{x: '0',autoAlpha:1, ease:Power4.easeInOut}, 0.1);
 
                 } else {
                   menuButton.classList.remove('cross');
                   let src = 'http://www.gcreativelab.com/wp-content/themes/gcreativelab_v2/assets/images/gcreative_logo.svg';
                   $('.glogo').attr('src', src);
+
+                  let menuIcon = 'http://www.gcreativelab.com/wp-content/themes/gcreativelab_v2/assets/images/menu-lines.svg';
+                  $('.glogo').attr('src', menuIcon);
                 }
               },
               resize: function(){ 

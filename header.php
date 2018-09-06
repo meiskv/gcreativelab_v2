522858e61833e846bd235e39aa68c233db61c377
Mikail Villamor <miks.villamor@yahoo.com>
2018-09-06 12:17:13 +0400

    header apple mobile

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 		<!-- Mobile Meta -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta class="foundation-mq">
 		
